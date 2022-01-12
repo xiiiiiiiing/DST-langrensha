1a64dc41bc4254031551bef80d86415ab1752b60
xiiiiiing <15908859309@163.com>
2022-01-12 18:39:48 +0800

    初始info文件

---
+++ modinfo.lua
@@ -0,0 +1,27 @@
+name = "Hunter"
+author = "X_x, Xiiiiiing"
+version = "1.0"
+description = ""
+forumthread = ""
+
+api_version = 10 --只适用于联机版仅适用于联机版
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+client_only_mod = false
+server_only_mod = true
+all_clients_require_mod = true
+
+--icon_atlas = "modicon.xml"
+--icon = "modicon.tex"
+--mod图标
+
+priority = 1
+
+server_filter_tags = {""}
+
+configuration_options = {}
+
